d2fd36c673354f9a0ce45d3b9183049ec572638a
Yuil <yuiltripathee79@gmail.com>
2019-07-11 22:39:32 +0545

    MailerBot v3.0
    * Almost near to complete automation with scalability.
    * Both options available, convenient and customized.
    * Extra message option in both bots.
    * Notification for which python file is being run.

---
+++ min/html_mail_writer.py
@@ -0,0 +1,214 @@
+# minified version of html_mail_writer.py, support script for mail sender bot
+
+import os
+import time
+from datetime import date
+
+# function to take week number to return week day (starting Monday as 0)
+
+
+def get_week_name(week_number):
+    if week_number == 0:
+        week_name = "Monday"
+    elif week_number == 1:
+        week_name = "Tuesday"
+    elif week_number == 2:
+        week_name = "Wednesday"
+    elif week_number == 3:
+        week_name = "Thursday"
+    elif week_number == 4:
+        week_name = "Friday"
+    elif week_number == 5:
+        week_name = "Saturday"
+    elif week_number == 6:
+        week_name = "Sunday"
+    else:
+        week_name = "Invalid weekday() input"
+    return week_name
+
+
+def write_mail(message):
+    hello()  # notifies initiation of mail writer script (bot engine)
+
+    # empty mail page container
+    mail_html_page = """"""
+    # initial part of mail page
+    html_page_part1 = """
+        <!DOCTYPE html>
+        <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+        <head>
+            <meta charset="utf-8"> <!-- utf-8 works for most cases -->
+            <meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
+            <meta name="x-apple-disable-message-reformatting">  <!-- Disable auto-scale in iOS 10 Mail entirely -->
+            <title>""" + message["title"] + """</title> <!-- The title tag shows in email notifications, like Android 4.4. -->
+
+        </head>
+
+        <body width="100%" style="margin: 0; padding: 0 !important; font-family: 'Courier New', Courier, monospace, sans-serif;">
+            <center style="width: 100%;">
+            <div style="display: none; font-size: 1px;max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+            &zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+            </div>
+            <div style="max-width: 600px; margin: 0 auto;">
+                <!-- BEGIN BODY -->
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
+                <tr>
+                    <td>
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                        <tr>
+                            <td>
+                                <div style="padding: 0 30px;">
+                                <h2 style="text-align: center;">""" + message['title'] + """</h2>
+                                <p>Hi,</p>
+                                <p>
+    """
+
+    # Adding initial part into the mail page
+    mail_html_page += html_page_part1  # initial section
+
+    # Adding message into the mail page
+    mail_html_page += message["message"]
+
+    # If you consider adding up extra text after a template (or either without using it.)
+    add_para_prompt = input(
+        "Do you need to add extra text other than tempate? [y/n]\n")
+    if add_para_prompt == 'y':
+        # like <b> for bold, <i> for italics and so on
+        print("\nFeel comfortable writing text in HTML format:")
+        print("===============================================\n")
+        add_contents = []
+        while True:
+            line = input()
+            if line == '':
+                break
+            add_contents.append(line)
+        add_content_para = "<p>" + '<br>'.join(add_contents) + "</p>"
+        mail_html_page += add_content_para
+    # If you consider not writing additional texts...
+    else:
+        pass
+
+    # For date of update
+    dateToday = date.today()
+    dateStr = "<b>" + str(dateToday.month) + "/" + str(dateToday.day) + "/" + str(dateToday.year) + " " + \
+        get_week_name(dateToday.weekday()) + "</b><br>Time: <b>" + \
+        time.strftime("%I:%M %p %z") + "</b>"
+    mail_html_page += "<p> Date: " + dateStr + "</p>"
+
+    # container for embedding images
+    files = []
+    for (path, dirnames, filenames) in os.walk('snapshots'):
+        files.extend(os.path.join(path, name) for name in filenames)
+    print("\nEmbedding image reference...")
+    if len(files) != 0:
+        mail_html_page += "<h3> Some snapshots </h3>"
+    for img_ci in range(0, len(files)):
+        image_ci = img_ci + 1
+        mail_html_page += '<p><img src="cid:image' + \
+            str(image_ci) + '" alt="Snapshot" style="max-width: 100%;"><br><i>' + \
+            files[img_ci] + '</i></p>'
+        print("<image:" + str(image_ci) + "> :"+files[img_ci])
+    print("\nImage reference added sucessfully!")
+    # footer part of the html mail page
+    html_page_part2 = """</p>
+                        </div>                       
+                      </td>
+                    </tr><!-- end: tr -->
+
+                  </table>
+
+                </td>
+              </tr><!-- end:tr -->
+            <!-- 1 Column Text + Button : END -->
+            </table>
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto; padding: 0 30px;">
+              <tr>
+                <td valign="middle">
+                  <table>
+                    <tr>
+                      <td valign="top" style="padding-top: 20px;">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                          <tr>
+                            <td style="text-align: left; padding-right: 10px;">
+                              <h3>About</h3>
+                              <p>""" + message["about"] + """</p><p><b>Thanks for watching</b></p>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr><!-- end: tr -->
+              
+            </table>
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto; padding: 0 30px;">
+              <tr>
+                <td valign="middle">
+                  <table>
+                    <tr>
+                      
+                      <td valign="top" style="padding-top: 20px;">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                          <tr>
+                            <td style="text-align: left; padding-left: 5px; padding-right: 5px;">
+                              <h3>Contact Info</h3>
+                              <img src="https://media.licdn.com/dms/image/C5103AQGPnRGooXJusQ/profile-displayphoto-shrink_800_800/0?e=1564617600&v=beta&t=YTb3XoDzPtnIPOgrOaqJZEYxWBTGxZ3RZF2eaaXhgVc" width="128px" alt="mypicture" style="border-radius: 10px;">
+                              <ul>
+                                <li><span>Yuil Tripathee <i>@YuilTripathee</i></span></li>
+                                <li><span>yuiltripathee79@gmail.com</span></li>
+                                <li><span>+977 (982)5158989</span></a></li>
+                              </ul>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                      
+                    </tr>
+                  </table>
+                </td>
+              </tr><!-- end: tr -->
+              
+            </table>
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto; padding: 0 30px;">
+              <tr>
+                <td valign="middle">
+                  <table>
+                    <tr>
+                      <td valign="top" style="padding-top: 20px;">
+                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                          <tr>
+                            <td style="text-align: left; padding-left: 10px;">
+                              <h3>Useful Links</h3>
+                              <p align="center"><a href="https://yuiltripathee.github.io/">Website</a>&nbsp; <a href="https://github.com/YuilTripathee">GitHub</a>&nbsp; <a href="https://www.linkedin.com/in/yuiltr/">LinkedIn</a>
+                              </p>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr><!-- end: tr -->
+              
+            </table>
+          </div>
+        </center>
+      </body>
+      </html>
+    """
+    mail_html_page += html_page_part2
+
+    # writing entire template into a file
+    # will keep this feature for testing reasons only
+    # with open('mail/min.html', 'w', encoding="utf-8") as mail_page_fp:
+    #     mail_page_fp.write(mail_html_page)
+    #     mail_page_fp.close()
+
+    print("\nMail page written sucessfully!")
+    return mail_html_page
+
+
+def hello(): # identifier for which python script is being executed
+    print('\nmin: ' + __file__ + '\n')
---
+++ min/sendmail.py
@@ -0,0 +1,172 @@
+# minified version of mail sender bot
+
+import os
+import sys
+import json
+from datetime import date
+import socket
+
+# custom library (downloaded from pip) to mask password inputs
+import stdiomask
+
+# Import smtplib for the actual sending function
+import smtplib
+
+# For guessing MIME type
+import mimetypes
+
+# Import the email modules we'll need
+import email
+import email.mime.application
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+
+# import custom written html mail writer
+import html_mail_writer as hwriter
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '..'))) # for importing file for parent directory (up A level directory)
+import mail_util # mail utility scripts
+
+# method for sending mail in MIME format
+def send_mail(sender, recipent, msg):
+    try:
+        with smtplib.SMTP('smtp.gmail.com:587') as mail_sys:
+            mail_sys.ehlo()
+            print("Established SMTP connection with the mail server!")
+            mail_sys.starttls()
+            print("TLS connection established!")
+            print(
+                "Yuil, you have to provide your e-mail password in order to login to SMTP.")
+            password = stdiomask.getpass()
+            mail_sys.login(sender, password)
+            print("Logged in sucessfully!")
+            print("Sending mail...")
+            mail_sys.sendmail(sender,
+                              recipent, msg.as_string())
+            print('Mail sent sucessfully')
+            pass
+    except smtplib.SMTPAuthenticationError:
+        print("\nUsername or password may be invalid.")
+        send_mail(sender, recipent, msg)
+    except socket.gaierror:
+        print("\nConnectivity Problem.")
+    except Exception:
+        print('Mail failed to transfer')
+        raise
+
+if __name__ == "__main__":
+    
+    print("\n" + __file__ + "\n")
+
+    # open the template file
+    with open('min/data/main.json', 'r', encoding='utf-8') as data_fp: # top level invoking requires locating file from the shell's localtion (i.e. project directory)
+        data = json.load(data_fp)
+        data_fp.close()
+
+    # selecting data template
+    print("\nAvailable templates:")
+    print("====================")
+    for c_i in range(0, len(data)):
+        print(str(c_i) + ": " + data[c_i]["ref"])
+    
+    # getting and validating data reference from JSON array
+    data_id = int(input("\nEnter the data 'id' for the required format: "))
+    try:
+        dataset = data[data_id]
+        print(dataset["ref"])
+        print("Data id: " + str(data_id) + " is available.")
+    except Exception:
+        raise
+
+    # start to compile mail
+    msg = MIMEMultipart()
+
+    # Initializing mail headers
+    msg["From"] = dataset["sender"]["name"] + " <" + dataset["sender"]["email"] + ">"
+    msg['To'] = dataset["recipent"]["name"] + " <" + dataset["recipent"]["email"] + ">"
+
+    # embedding CC list to mail header
+    cc_list = dataset["cc"]
+    if cc_list != []:
+        cc_list_string = ""
+        # converting CC data to MIME usable format
+        for cc_unit in cc_list:
+            cc_list_string += cc_unit["name"] + " <" + cc_unit["email"] + ">, "
+        msg['CC'] = cc_list_string
+    else:
+        cc_list == []
+
+    # inserting mail subject
+    mail_title = dataset["message"]["title"]
+    dateToday = date.today() # for date of update mail sent
+    msg['Subject'] = mail_title + ": " + str(dateToday.month) + "/" + \
+        str(dateToday.day) + "/" + str(dateToday.year)
+    
+    print("Headers initialized sucessfully")
+    print("Writing your email!")
+    # run html_mail_writer.py to write HTML mail into a file (saving is done for backup purposes)
+    mail_page = hwriter.write_mail(dataset["message"])
+
+    # # importing HTML page for mail
+    # with open('mail/min.html', 'r', encoding='utf-8') as mailPage_fp:
+    #     mail_page = mailPage_fp.read().replace('\n', '')
+    #     mailPage_fp.close()
+    
+    # embedding HTML file into the mail
+    mail_html_content = MIMEText(mail_page, 'html')
+    msg.attach(mail_html_content)
+
+    # Embedding images into the mail
+    # Getting the list of paths for the image in snapshots folder
+    files = []
+    print("\nInserting images:")
+    print("=================")
+    
+    # accessing files and appending into file list
+    for (path, dirnames, filenames) in os.walk('snapshots'):
+        files.extend(os.path.join(path, name) for name in filenames)
+    
+    # writing each image file into mail page
+    for imge_ci in range(0, len(files)):
+        image_ci = str(imge_ci + 1) # special numbering for content id in HTML page image cid
+        img_fp = open(files[imge_ci], 'rb') # image file binary for network stream
+        img_filename = os.path.basename(files[imge_ci]) # file name of the particular image file
+        msgImage = MIMEImage(img_fp.read(), img_filename) # encoding image into MIME format
+        img_fp.close()
+        image_value = "<image" + image_ci + ">"
+        msgImage.add_header('Content-ID', image_value) # MIME header for Content-ID
+        msgImage.add_header('Content-Disposition', # MIME header for Content-Disposition
+                            'inline', filename=img_filename)
+        msg.attach(msgImage) # attaching image to the main MIME email object
+        print("Image inserted: " + files[imge_ci])
+    
+    # Inserting mail attachments
+    attaches = []
+    print("\nAttaching files:")
+    print("================")
+    # Getting down the list
+    for (path, dirnames, filenames) in os.walk('attachments'):
+        attaches.extend(os.path.join(path, name) for name in filenames)
+    # Attaching files into the Mail
+    for attach_ci in range(0, len(attaches)):
+        # file path for the file to be attached
+        attach_file = attaches[attach_ci]
+        attach_file_fp = open(attach_file, 'rb')  # file binary to be attached
+        # file name of the file to be attached
+        attFileName = os.path.basename(attach_file)
+        # encoding file for attachment
+        attFile = MIMEApplication(attach_file_fp.read(), Name=attFileName)
+        attach_file_fp.close()  # closing the file pointer
+        attFile.add_header('Content-Disposition', 'attachment',
+                           filename=attFileName)  # adding the attachment file
+        msg.attach(attFile)
+        print("File attached: " + attach_file)
+    
+    # Firing up the mail engine
+    print("\nSending mail...")
+    send_mail(dataset["sender"]["email"], dataset["recipent"]["email"], msg)
+    return_string = input("Press any key to continue")
+
+    
